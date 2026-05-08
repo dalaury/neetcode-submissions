d7bfbd232d035313e0e55a791800e7a9c66f7903
DeJuan Laury <54858269+dalaury@users.noreply.github.com>
2026-05-07 21:32:03 -0700

    Add: python-sort-custom - submission-0

---
+++ Python Coding Interviews/python-sort-custom/submission-0.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:
+    def get_word_length(word: str) -> int:
+        return len(word)
+
+    words.sort(key=get_word_length, reverse=True)
+
+    return words
+
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    def get_abs_value(number):
+        return abs(number)
+
+    numbers.sort(key=get_abs_value)
+
+    return numbers
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]))
